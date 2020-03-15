055a083f0a062bd9fb4333300cf5f1665be8ac4d
Vagus-art <agustin.favo@gmail.com>
2020-03-14 23:35:45 -0300

    reform route error handling

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,28 +1,30 @@
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
 const contactValidationRules = () => {
-    return [
-      // username must be an email
-      body('name').isLength({ min: 5 }),
-      // password must be at least 5 chars long
-      body('phone').isInt().isLength({ min: 5 }),
-    ]
+  return [
+    // username must be an email
+    body("name").isLength({ min: 5 }),
+    // password must be at least 5 chars long
+    body("phone")
+      .isInt()
+      .isLength({ min: 5 })
+  ];
+};
+
+const validate = (request, response, next) => {
+  const errors = validationResult(request);
+  if (errors.isEmpty()) {
+    return next();
   }
-  
-  const validate = (request, response, next) => {
-    const errors = validationResult(request)
-    if (errors.isEmpty()) {
-      return next()
-    }
-    const extractedErrors = []
-    errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
-  
-    return response.json({
-      errors: extractedErrors,
-    })
-  }
-  
-  module.exports = {
-    contactValidationRules,
-    validate,
-  }
+  const extractedErrors = [];
+  errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+
+  return response.json({
+    errors: extractedErrors
+  });
+};
+
+module.exports = {
+  contactValidationRules,
+  validate
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
 const router = Router();
+const { UNIQUE_VIOLATION } = require('pg-error-constants');
+
+//conexion a base de datos por variables de entorno
 const db = require("../database");
+
+//para validar post y put
 const { contactValidationRules, validate } = require("../helpers/validator");
 
+//para validar param de id
+const { param } = require("express-validator");
+
+
+//OBTENER TODOS LOS CONTACTOS
 router.get("/", (request, response) => {
   return db.query("SELECT * FROM contacts", (err, res) => {
     if (err) {
@@ -13,7 +23,9 @@ router.get("/", (request, response) => {
   });
 });
 
-router.get("/:id", (request, response) => {
+
+//OBTENER CONTACTO POR ID
+router.get("/:id", [param('id').isInt()], validate, (request, response) => {
   const { id } = request.params;
   return db.query(`SELECT * FROM contacts WHERE id=${id}`, (err, res) => {
     if (err) {
@@ -24,29 +36,19 @@ router.get("/:id", (request, response) => {
   });
 });
 
+
+//CREAR NUEVO CONTACTO
 router.post("/", contactValidationRules(), validate, (request, response) => {
   const { name, phone } = request.body;
   db.query(
     `INSERT INTO contacts (name,phone) VALUES ('${name}',${phone})`,
     (err, res) => {
       if (err) {
-        switch (err.code) {
-          case 23505:
-            switch (err.constraint) {
-              case "contacts_phone_key":
-                response.status(400).json({ errors: [{name:"phone already exist"}] });
-                break;
-              default:
-                response.status(400).json({
-                  errors: [{name:"there is duplicate data in your form"}]
-                });
-                break;
-            }
-          default:
-            response.status(400).json({
-              errors: [{name:"there has been an error accessing our database servers"}]
-            });
-            break;
+        if (err.code==UNIQUE_VIOLATION&&err.constraint=="contacts_phone_key"){
+            response.status(400).json({errors:[{name:"That phone already exist in our databases..."}]});
+        }
+        else{
+            response.status(400).json({errors:[{name:"There has been an error with your form..."}]});
         }
       } else {
         const query = res;
