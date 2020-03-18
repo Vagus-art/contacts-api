382065f9266f6556fe3a800e305495981aec4648
Vagus-art <agustin.favo@gmail.com>
2020-03-18 19:02:02 -0300

    validators reform, abstracted copied chains

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,29 +1,24 @@
 const { body, param, validationResult } = require("express-validator");
 
-//VALIDACION DE PARAMETRO ID EN GET (contacts)
-const contactIdValidationRules = () => {
+//VALIDACION DE ID
+//PARAM
+const idParamValidationRules = () => {
   return [
     param("id")
       .isInt()
+      .trim()
+      .escape()
       .withMessage("ID must be INT")
   ];
 };
-
-//VALIDACION DE PARAMETRO OFFSET EN GET (contacts)
-const contactOffsetValidationRules = () => {
+//BODY
+const idBodyValidationRules = () => {
   return [
-    param("offset")
+    body("id")
       .isInt()
-      .withMessage("OFFSET must be INT")
-  ];
-};
-
-//VALIDACION DE PARAMETROS BUSQUEDA (contacts)
-const contactSearchValidationRules = () => {
-  return [
-    param("name")
       .trim()
       .escape()
+      .withMessage("ID must be INT")
   ];
 };
 
@@ -48,45 +43,46 @@ const contactValidationRules = () => {
   ];
 };
 
+//VALIDACION DE PARAMETRO ID EN GET (contacts)
+const contactIdValidationRules = () => {
+  return idParamValidationRules();
+};
+
+//VALIDACION DE PARAMETRO OFFSET EN GET (contacts)
+const contactOffsetValidationRules = () => {
+  return [
+    param("offset")
+      .isInt()
+      .withMessage("OFFSET must be INT")
+  ];
+};
+
+//VALIDACION DE PARAMETROS BUSQUEDA (contacts)
+const contactSearchValidationRules = () => {
+  return [
+    param("name")
+      .trim()
+      .escape()
+  ];
+};
+
 //VALIDACION DE FORMULARIO PUT/UPDATE (contacts)
 const updateContactValidationRules = () => {
   return [
-    body("name")
-      .isLength({ min: 5 })
-      .trim()
-      .escape()
-      .withMessage("Name must be at least 5 characters long..."),
-    body("phone")
-      .isInt()
-      .isLength({ min: 5 })
-      .trim()
-      .escape()
-      .withMessage("Phone number must be 5 digits long and only numbers..."),
-    body("id")
-      .isInt()
-      .trim()
-      .escape()
-      .withMessage("Identifier is not valid")
+    ...contactIdValidationRules(),
+    ...idBodyValidationRules()
   ];
 };
 
 //VALIDACION DE PARAMETROS DELETE (contacts)
 const deleteContactValidationRules = () => {
-  return [
-    param("id")
-      .isInt()
-      .trim()
-      .escape()
-      .withMessage("ID must be INT")
-  ];
+  return idParamValidationRules();
 };
 
 //VALIDACION DE FORMULARIO DELETE POR ARREGLO (contacts)
 const deleteContactArrayValidationRules = () => {
   return [
-    body("id")
-      .isArray()
-      .withMessage("ID must be an array of INT"),
+    ...idBodyValidationRules(),
     body("id.*")
       .isInt()
       .withMessage("ID must be an array of INT")
